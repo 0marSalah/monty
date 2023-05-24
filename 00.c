c7aff108e5e340668c294aeb2c77a526642caae3
0marSalah <omar.salah1597@gmail.com>
2023-05-24 15:57:41 +0300

    fix betty

--- 00.c
+++ 00.c
@@ -9,43 +9,51 @@
 int stack[STACK_SIZE];
 int top = -1;
 
-int main(int argc, char *argv[]) {
-    FILE *file;
-    char line[256];
-    int line_number = 1;
-    char *opcode, *arg;
+int main(int argc, char *argv[])
+{
+FILE *file;
+char line[256];
+int line_number = 1;
+char *opcode, *arg;
 
-    if (argc != 2) {
-        printf("USAGE: monty %s\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+if (argc != 2)
+{
+printf("USAGE: monty %s\n", argv[0]);
+exit(EXIT_FAILURE);
+}
 
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("Error: Can't open file %s\\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+file = fopen(argv[1], "r");
+if (file == NULL) {
+printf("Error: Can't open file %s\\n", argv[1]);
+exit(EXIT_FAILURE);
+}
 
-    while (fgets(line, sizeof(line), file) != NULL) {
-        line[strcspn(line, "\n")] = '\0';
+while (fgets(line, sizeof(line), file) != NULL)
+{
+line[strcspn(line, "\n")] = '\0';
 
-        opcode = strtok(line, " ");
-        arg = strtok(NULL, " ");
+opcode = strtok(line, " ");
+arg = strtok(NULL, " ");
 
-        if (opcode != NULL) {
-            if (strcmp(opcode, "push") == 0) {
-                push(line_number, arg);
-            } else if (strcmp(opcode, "pall") == 0) {
-                pall();
-            } else {
-                printf("L%d: unknown instruction %s\n", line_number, opcode);
-                return EXIT_FAILURE;
-            }
-        }
+if (opcode != NULL) {
+if (strcmp(opcode, "push") == 0)
+{
+push(line_number, arg);
+}
+else if (strcmp(opcode, "pall") == 0)
+{
+pall();
+}
+else
+{
+printf("L%d: unknown instruction %s\n", line_number, opcode);
+return EXIT_FAILURE;
+}
+}
 
-        line_number++;
-    }
+line_number++;
+}
 
-    fclose(file);
-    return (EXIT_SUCCESS);
+fclose(file);
+return (EXIT_SUCCESS);
 }
